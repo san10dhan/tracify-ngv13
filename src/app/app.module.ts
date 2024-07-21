bf4e658e77c35a23d555f6b112a954e0f1a1f0d9
Sanket <san10dhan@gmail.com>
2024-07-21 16:43:31 +0530

    Basic routing added for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
